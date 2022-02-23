e230942aae5b77b8059abd558ea32737922b3f0f
Giuseppe Tudisco <giuseppe.tudisco@inaf.it>
2022-02-23 10:48:03 +0100

    vtkWindowCube: Slice visualization

--- Code/src/vtkwindowcube.cpp
+++ Code/src/vtkwindowcube.cpp
@@ -1,6 +1,7 @@
 #include "vtkwindowcube.h"
 #include "ui_vtkwindowcube.h"
 
+#include "luteditor.h"
 #include "vtkfitsreader.h"
 #include "vtklegendscaleactor.h"
 
@@ -11,17 +12,26 @@
 #include <vtkFlyingEdges3D.h>
 #include <vtkFrustumSource.h>
 #include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkImageActor.h>
+#include <vtkImageMapToWindowLevelColors.h>
+#include <vtkLODActor.h>
+#include <vtkLookupTable.h>
 #include <vtkOrientationMarkerWidget.h>
 #include <vtkOutlineFilter.h>
 #include <vtkPlanes.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkProperty.h>
 #include <vtkRenderer.h>
+#include <vtkResliceImageViewer.h>
 #include <vtkTextActor.h>
-#include <vtkTextProperty.h>
 
-vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fitsReader)
-    : QMainWindow(parent), ui(new Ui::vtkWindowCube), fitsReader(fitsReader)
+vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fitsReader,
+                             QString velocityUnit)
+    : QMainWindow(parent),
+      ui(new Ui::vtkWindowCube),
+      fitsReader(fitsReader),
+      currentSlice(0),
+      velocityUnit(velocityUnit)
 {
     ui->setupUi(this);
     setAttribute(Qt::WA_DeleteOnClose);
@@ -30,14 +40,15 @@ vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fit
     fitsReader->is3D = true;
     fitsReader->CalculateRMS();
 
-    auto renWin = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    // Start datacube pipeline
+    auto renWinCube = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
     ui->qVtkCube->setDefaultCursor(Qt::ArrowCursor);
-    ui->qVtkCube->setRenderWindow(renWin);
+    ui->qVtkCube->setRenderWindow(renWinCube);
 
-    auto renderer = vtkSmartPointer<vtkRenderer>::New();
-    renderer->SetBackground(0.21, 0.23, 0.25);
-    renderer->GlobalWarningDisplayOff();
-    renWin->AddRenderer(renderer);
+    auto rendererCube = vtkSmartPointer<vtkRenderer>::New();
+    rendererCube->SetBackground(0.21, 0.23, 0.25);
+    rendererCube->GlobalWarningDisplayOff();
+    renWinCube->AddRenderer(rendererCube);
 
     double threshold = 3 * fitsReader->GetRMS();
 
@@ -79,7 +90,7 @@ vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fit
     frustumSource->Update();
     auto planeMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
     planeMapper->SetInputData(frustumSource->GetOutput());
-    auto planeActor = vtkSmartPointer<vtkActor>::New();
+    planeActor = vtkSmartPointer<vtkActor>::New();
     planeActor->SetMapper(planeMapper);
 
     // Axes
@@ -92,7 +103,7 @@ vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fit
     axesActor->GetYAxisCaptionActor2D()->GetTextActor()->SetTextScaleMode(0);
     axesActor->GetZAxisCaptionActor2D()->GetTextActor()->SetTextScaleMode(0);
     axesWidget = vtkSmartPointer<vtkOrientationMarkerWidget>::New();
-    axesWidget->SetInteractor(renWin->GetInteractor());
+    axesWidget->SetInteractor(renWinCube->GetInteractor());
     axesWidget->SetOrientationMarker(axesActor);
     axesWidget->SetOutlineColor(0.9300, 0.5700, 0.1300);
     axesWidget->SetViewport(0.0, 0.0, 0.2, 0.2);
@@ -100,20 +111,99 @@ vtkWindowCube::vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fit
     axesWidget->InteractiveOff();
 
     // Legend
-    auto legendActor = vtkSmartPointer<vtkLegendScaleActor>::New();
-    legendActor->LegendVisibilityOff();
-    legendActor->setFitsFile(fitsReader);
+    auto legendActorCube = vtkSmartPointer<vtkLegendScaleActor>::New();
+    legendActorCube->LegendVisibilityOff();
+    legendActorCube->setFitsFile(fitsReader);
 
-    renderer->AddActor(outlineActor);
-    renderer->AddActor(isosurfaceActor);
-    renderer->AddActor(planeActor);
-    renderer->AddActor(legendActor);
+    rendererCube->AddActor(outlineActor);
+    rendererCube->AddActor(isosurfaceActor);
+    rendererCube->AddActor(planeActor);
+    rendererCube->AddActor(legendActorCube);
 
-    renderer->ResetCamera();
-    renWin->GetInteractor()->Render();
+    rendererCube->ResetCamera();
+    renWinCube->GetInteractor()->Render();
+    // End datacube pipeline
+
+    // Start slice pipeline
+    auto renWinSlice = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    ui->qVtkSlice->setDefaultCursor(Qt::ArrowCursor);
+    ui->qVtkSlice->setRenderWindow(renWinSlice);
+
+    auto rendererSlice = vtkSmartPointer<vtkRenderer>::New();
+    rendererSlice->SetBackground(0.21, 0.23, 0.25);
+    rendererSlice->GlobalWarningDisplayOff();
+    renWinSlice->AddRenderer(rendererSlice);
+
+    auto lutSlice = vtkSmartPointer<vtkLookupTable>::New();
+    lutSlice->SetTableRange(fitsReader->GetRangeSlice(0)[0], fitsReader->GetRangeSlice(0)[1]);
+    SelectLookTable("Gray", lutSlice);
+
+    /// \todo Interactor Image Contour
+
+    sliceViewer = vtkSmartPointer<vtkResliceImageViewer>::New();
+    sliceViewer->SetInputData(fitsReader->GetOutput());
+    sliceViewer->GetWindowLevel()->SetOutputFormatToRGB();
+    sliceViewer->GetWindowLevel()->SetLookupTable(lutSlice);
+    sliceViewer->GetImageActor()->InterpolateOff();
+    sliceViewer->SetRenderWindow(renWinSlice);
+    sliceViewer->SetRenderer(rendererSlice);
+
+    ui->sliceSlider->setRange(1, fitsReader->GetNaxes(2));
+    ui->sliceSpinBox->setRange(1, fitsReader->GetNaxes(2));
+
+    auto legendActorSlice = vtkSmartPointer<vtkLegendScaleActor>::New();
+    legendActorSlice->LegendVisibilityOff();
+    legendActorSlice->setFitsFile(fitsReader);
+    rendererSlice->AddActor(legendActorSlice);
+
+    rendererSlice->ResetCamera();
+    renWinSlice->GetInteractor()->Render();
 }
 
 vtkWindowCube::~vtkWindowCube()
 {
     delete ui;
+}
+
+void vtkWindowCube::on_sliceSlider_valueChanged(int value)
+{
+    ui->sliceSpinBox->setValue(value);
+    currentSlice = value - 1;
+    updateVelocityText();
+    updateSliceDatacube();
+}
+
+void vtkWindowCube::on_sliceSpinBox_valueChanged(int value)
+{
+    ui->sliceSlider->setValue(value);
+}
+
+void vtkWindowCube::updateSliceDatacube()
+{
+    planeActor->SetPosition(0, 0, currentSlice + 1);
+
+    float *range = fitsReader->GetRangeSlice(currentSlice);
+    ui->minSliceText->setText(QString::number(range[0], 'f', 4));
+    ui->maxSliceText->setText(QString::number(range[1], 'f', 4));
+
+    auto lutSlice = vtkSmartPointer<vtkLookupTable>::New();
+    lutSlice->SetTableRange(range[0], range[1]);
+    SelectLookTable("Gray", lutSlice);
+
+    sliceViewer->GetWindowLevel()->SetLookupTable(lutSlice);
+    sliceViewer->SetSlice(currentSlice);
+
+    sliceViewer->GetRenderer()->ResetCamera();
+    sliceViewer->Render();
+    ui->qVtkCube->renderWindow()->GetInteractor()->Render();
+}
+
+void vtkWindowCube::updateVelocityText()
+{
+    double velocity = fitsReader->getInitSlice() + fitsReader->GetCdelt(2) * currentSlice;
+    if (velocityUnit.startsWith("m")) {
+        // Return value in km/s
+        velocity /= 1000;
+    }
+    ui->velocityText->setText(QString::number(velocity).append(" Km/s"));
 }
--- Code/src/vtkwindowcube.h
+++ Code/src/vtkwindowcube.h
@@ -5,7 +5,9 @@
 
 #include <vtkSmartPointer.h>
 
+class vtkActor;
 class vtkFitsReader;
+class vtkResliceImageViewer;
 class vtkOrientationMarkerWidget;
 
 namespace Ui {
@@ -17,13 +19,26 @@ class vtkWindowCube : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fitsReader);
+    explicit vtkWindowCube(QWidget *parent, vtkSmartPointer<vtkFitsReader> fitsReader,
+                           QString velocityUnit = "km/s");
     ~vtkWindowCube();
+
+private slots:
+    void on_sliceSlider_valueChanged(int value);
+    void on_sliceSpinBox_valueChanged(int value);
 
 private:
     Ui::vtkWindowCube *ui;
     vtkSmartPointer<vtkFitsReader> fitsReader;
+    int currentSlice;
+    QString velocityUnit;
+
+    vtkSmartPointer<vtkActor> planeActor;
     vtkSmartPointer<vtkOrientationMarkerWidget> axesWidget;
+    vtkSmartPointer<vtkResliceImageViewer> sliceViewer;
+
+    void updateSliceDatacube();
+    void updateVelocityText();
 };
 
 #endif // VTKWINDOWCUBE_H
