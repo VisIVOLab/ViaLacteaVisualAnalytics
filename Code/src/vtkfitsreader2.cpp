07d7e8cda192a6c838b1178baf92185d34370f6d
Giuseppe Tudisco <giuseppe.tudisco@inaf.it>
2023-02-03 11:05:29 +0100

    Fix crash when loading datacubes that have more than 3 axes

--- Code/src/vtkfitsreader2.cpp
+++ Code/src/vtkfitsreader2.cpp
@@ -12,6 +12,7 @@
 #include <vtkPointData.h>
 #include <vtkStreamingDemandDrivenPipeline.h>
 
+#include <algorithm>
 #include <cmath>
 
 vtkStandardNewMacro(vtkFitsReader2);
@@ -75,7 +76,6 @@ int vtkFitsReader2::RequestInformation(vtkInformation *vtkNotUsed(request),
         delete[] naxes;
         return 0;
     }
-    this->NumberOfSlices = naxes[2];
 
     if (fits_close_file(fptr, &ReadStatus)) {
         std::cerr << "vtkFitsReader2::RequestInformation [CFITSIO] Error fits_close_file";
@@ -83,21 +83,21 @@ int vtkFitsReader2::RequestInformation(vtkInformation *vtkNotUsed(request),
         // We should have axes information, so we do not abort (i.e. no return failure here)
     }
 
-    int dataExtent[6];
-    for (int axis = 0; axis < naxis; ++axis) {
-        dataExtent[2 * axis] = 0;
-        dataExtent[2 * axis + 1] = (naxes[axis] - 1) / this->ScaleFactor;
-    }
-    delete[] naxes;
-
+    int dataExtent[6] = { 0, 0, 0, 0, 0, 0 };
+    this->NumberOfSlices = naxes[2];
+    dataExtent[1] = (naxes[0] - 1) / this->ScaleFactor;
+    dataExtent[3] = (naxes[1] - 1) / this->ScaleFactor;
     if (this->SliceMode) {
         dataExtent[4] = this->Slice;
         dataExtent[5] = this->Slice;
+    } else {
+        dataExtent[5] = (naxes[2] - 1) / this->ScaleFactor;
     }
 
     vtkInformation *outInfo = outputVector->GetInformationObject(0);
     outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), dataExtent, 6);
 
+    delete[] naxes;
     return 1;
 }
 
@@ -105,9 +105,13 @@ int vtkFitsReader2::RequestData(vtkInformation *vtkNotUsed(request),
                                 vtkInformationVector **vtkNotUsed(inputVector),
                                 vtkInformationVector *outputVector)
 {
-    int dataExtent[6] = { 0, -1, 0, -1, 0, -1 };
+    int dataExtent[6];
     vtkInformation *outInfo = outputVector->GetInformationObject(0);
     outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(), dataExtent);
+
+    long dimX = dataExtent[1] - dataExtent[0] + 1;
+    long dimY = dataExtent[3] - dataExtent[2] + 1;
+    long dimZ = dataExtent[5] - dataExtent[4] + 1;
 
     outData = vtkImageData::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
     outData->SetExtent(dataExtent);
@@ -122,13 +126,35 @@ int vtkFitsReader2::RequestData(vtkInformation *vtkNotUsed(request),
         return 0;
     }
 
-    long dimX = dataExtent[1] - dataExtent[0] + 1;
-    long dimY = dataExtent[3] - dataExtent[2] + 1;
-    long dimZ = dataExtent[5] - dataExtent[4] + 1;
-    long fP[] = { dataExtent[0] + 1, dataExtent[2] + 1, dataExtent[4] + 1 };
-    long lP[] = { ScaleFactor * dataExtent[1] + 1, ScaleFactor * dataExtent[3] + 1,
-                  ScaleFactor * dataExtent[5] + 1 };
-    long inc[] = { ScaleFactor, ScaleFactor, ScaleFactor };
+    // DO NOT assume datacubes have exactly 3 axes, they may have more
+    int naxis = 0;
+    if (fits_get_img_dim(fptr, &naxis, &ReadStatus)) {
+        std::cerr << "vtkFitsReader2::RequestData [CFITSIO] Error fits_get_img_dim";
+        fits_report_error(stderr, ReadStatus);
+        return 0;
+    }
+
+    // First Pixel coords
+    long *fP = new long[naxis];
+    std::fill_n(fP, naxis, 1);
+    fP[0] = dataExtent[0] + 1;
+    fP[1] = dataExtent[2] + 1;
+    fP[2] = dataExtent[4] + 1;
+
+    // Last Pixel coords
+    long *lP = new long[naxis];
+    std::fill_n(lP, naxis, 1);
+    lP[0] = ScaleFactor * dataExtent[1] + 1;
+    lP[1] = ScaleFactor * dataExtent[3] + 1;
+    lP[2] = ScaleFactor * dataExtent[5] + 1;
+
+    // Increments
+    long *inc = new long[naxis];
+    std::fill_n(inc, naxis, 1);
+    inc[0] = ScaleFactor;
+    inc[1] = ScaleFactor;
+    inc[2] = ScaleFactor;
+
     float nulval = 1e-30;
     int anynul = 0;
     long nels = dimX * dimY * dimZ;
