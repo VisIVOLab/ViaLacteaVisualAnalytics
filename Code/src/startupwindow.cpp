f4111e44a0a5acfd1f7a5e16c4fbb6f3f5f74f99
Fabio Vitello <fvitello@users.noreply.github.com>
2024-04-30 12:35:08 +0200

    changed background color

--- Code/src/startupwindow.cpp
+++ Code/src/startupwindow.cpp
@@ -6,6 +6,24 @@ StartupWindow::StartupWindow(QWidget *parent) :
     ui(new Ui::StartupWindow)
 {
     ui->setupUi(this);
+
+    QPalette pal = QPalette();
+    pal.setColor(QPalette::Window, QColor::fromRgb(234,233,229));
+    ui->leftContainer->setAutoFillBackground(true);
+    ui->leftContainer->setPalette(pal);
+
+    QPalette pal2 = QPalette();
+    pal2.setColor(QPalette::Window, Qt::white);
+
+    ui->logoArea->setAutoFillBackground(true);
+    ui->logoArea->setPalette(pal2);
+
+    ui->historyArea->setAutoFillBackground(true);
+    ui->historyArea->setPalette(pal2);
+
+    ui->buttonArea->setAutoFillBackground(true);
+    ui->buttonArea->setPalette(pal2);
+
 }
 
 StartupWindow::~StartupWindow()
