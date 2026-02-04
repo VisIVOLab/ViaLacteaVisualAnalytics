a7e7c0205d3236eac4a5b2f58f6f59e34357eda8
Giuseppe Tudisco <giuseppe.tudisco@inaf.it>
2026-02-04 15:06:38 +0100

    Use new app logo

--- src/gui/AboutDialog.cpp
+++ src/gui/AboutDialog.cpp
@@ -7,7 +7,7 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent), ui(new Ui::AboutDia
 {
     ui->setupUi(this);
 
-    ui->labelLogo->setPixmap(QPixmap(u":/icons/VisIVO.svg"_s));
+    ui->labelLogo->setPixmap(QPixmap(u":/icons/VisIVO_512.png"_s));
     ui->label->setText(u"%1 v%2"_s.arg(qApp->applicationName(), qApp->applicationVersion()));
 }
 
